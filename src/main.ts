f68e7e53084bd5f8fe781429906fb4e109e683f6
Julian Pömp <jp@poemp.net>
2024-08-30 08:36:18 +0200

    music-metadata 10.3.0 not working

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import { parseFile, parseStream } from 'music-metadata';
+import { createReadStream } from 'node:fs';
+
+async function main() {
+  try {
+    // Create a readable stream from a file
+    const audioStream = createReadStream('src/assets/EinsZweiDrei.wav');
+
+    // Parse the metadata from the stream
+    const metadata = await parseStream(audioStream, { mimeType: 'audio/mpeg'});
+
+    // Log the parsed metadata
+    console.log(metadata);
+  } catch (error) {
+    console.error('Error parsing metadata:', error.message);
+  }
+}
+
+main();
